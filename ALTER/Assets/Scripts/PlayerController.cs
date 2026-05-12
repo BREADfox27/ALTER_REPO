ef149e4242d9333f68cc38ae9eb5c00d31340103
BREADfox27 <javierluna271107@gmail.com>
2026-05-12 23:22:08 +0200

    Camera

--- ALTER/Assets/Scripts/PlayerController.cs
+++ ALTER/Assets/Scripts/PlayerController.cs
@@ -21,10 +21,7 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        //Vector3 dir = new Vector3(h, 0, v); Victoria's Code
-
-        // Javier's Code (only line below)
-        Vector3 dir = transform.right * h + transform.forward * v;
+        Vector3 dir = new Vector3(h, 0, v);
 
         // Movimiento
         if (dir.magnitude > 0.1f)
