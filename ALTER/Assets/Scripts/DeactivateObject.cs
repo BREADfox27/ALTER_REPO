4daf990ed1f3b293ef3454392243bf0b200b19e0
BREADfox27 <javierluna271107@gmail.com>
2026-05-14 18:53:51 +0200

    Dialogue System Fix

--- ALTER/Assets/Scripts/DeactivateObject.cs
+++ ALTER/Assets/Scripts/DeactivateObject.cs
@@ -2,7 +2,6 @@
 
 public class DeactivateObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Deactivate()
     {
         Destroy(gameObject);
