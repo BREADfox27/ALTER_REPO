95677073a830a8dfd15794e8e2280287416e33f5
Vi0325 <viv2503h@gmail.com>
2026-05-17 17:01:34 +0200

    Basic Audio Manager Script

--- ALTER/Assets/Scripts/AudioManager.cs
+++ ALTER/Assets/Scripts/AudioManager.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static AudioManager Instance;
+
+    [Header("Audio Clip Arrays")]
+    public AudioClip[] musicList;
+    public AudioClip[] sfxList;
+
+    [Header("Audio Source References")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private int musicToPlay;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        PlayMusic(musicToPlay);
+    }
+
+    public void PlayMusic(int musicIndex)
+    {
+        if (musicIndex >= 0 && musicIndex < musicList.Length)
+        {
+            musicSource.clip = musicList[musicIndex];
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(int sfxIndex)
+    {
+        if (sfxIndex >= 0 && sfxIndex < sfxList.Length)
+        {
+            sfxSource.PlayOneShot(sfxList[sfxIndex]);
+        }
     }
 }
